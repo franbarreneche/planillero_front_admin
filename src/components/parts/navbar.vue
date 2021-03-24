d282a4662b3948485940232882731d68e632355f
Francisco Barreneche <franbarreneche@gmail.com>
2021-03-24 19:41:31 -0300

    implementacion navbar

---
+++ src/components/parts/navbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <b-navbar class="is-dark">
+        <template #brand>
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <img
+                    src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+                    alt="Lightweight UI components for Vue.js based on Bulma"
+                >
+            </b-navbar-item>
+        </template>
+        <template #start>
+            <b-navbar-item tag="router-link" :to="{name : 'Home'}">
+                Home
+            </b-navbar-item>
+            <b-navbar-item tag="router-link" :to="{name : 'Torneos'}">
+                Torneos
+            </b-navbar-item>
+            <b-navbar-item tag="router-link" :to="{name : 'Partidos'}">
+                Partidos
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+    name : "Navbar"
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/AdminPartidos.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="title">PARTIDOS</h1>
+</template>
+
+<script>
+export default {
+  name : 'AdminPartidos'
+}
+</script>
+
+<style>
+
+</style>
